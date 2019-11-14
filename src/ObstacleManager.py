f0d82671e4875ec0e61d251c425f7cb47ccf86f2
Steve Lambert <steverlambert@gmail.com>
2019-11-13 16:11:51 -0800

    Initial commit/copy from uw

---
+++ src/ObstacleManager.py
@@ -0,0 +1,70 @@
+import cv2
+import math
+import numpy
+import Utils
+
+class ObstacleManager(object):
+
+	def __init__(self, mapMsg, car_width, car_length, collision_delta):
+		self.map_info = mapMsg.info
+		self.mapImageGS = numpy.array(mapMsg.data, dtype=numpy.uint8).reshape(
+			(mapMsg.info.height, mapMsg.info.width, 1))
+
+		# Retrieve the map dimensions
+		height, width, channels = self.mapImageGS.shape
+		self.mapHeight = height
+		self.mapWidth = width
+		self.mapChannels = channels
+
+		# Binarize the Image
+		self.mapImageBW = 255 * numpy.ones_like(self.mapImageGS, dtype=numpy.uint8)
+		self.mapImageBW[self.mapImageGS == 0] = 0
+
+		# Obtain the car length and width in pixels
+		self.robotWidth = int(car_width / self.map_info.resolution + 0.5)
+		self.robotLength = int(car_length / self.map_info.resolution + 0.5)
+		self.collision_delta = collision_delta
+
+	# Check if the passed config is in collision
+	# config: The configuration to check (in meters and radians)
+	# Returns False if in collision, True if not in collision
+	def get_state_validity(self, config):
+
+		# Convert the configuration to map-coordinates -> mapConfig is in pixel-space
+		mapConfig = Utils.world_to_map(config, self.map_info)
+
+		# ---------------------------------------------------------
+		# YOUR CODE HERE
+		#
+		# Return true or false based on whether the robot's configuration is in collision
+		# Use a square to represent the robot, return true only when all points within
+		# the square are collision free
+		#
+		# Also return false if the robot is out of bounds of the map
+		#
+		# Although our configuration includes rotation, assume that the
+		# square representing the robot is always aligned with the coordinate axes of the
+		# map for simplicity
+		# ----------------------------------------------------------
+		return True
+
+	# Check if there is an unobstructed edge between the passed configs
+	# config1, config2: The configurations to check (in meters and radians)
+	# Returns false if obstructed edge, True otherwise
+	def get_edge_validity(self, config1, config2):
+		# -----------------------------------------------------------
+		# YOUR CODE HERE
+		#
+		# Check if endpoints are obstructed, if either is, return false
+		# Find path between two configs by connecting them with a straight line
+		# Discretize the path into N configurations, where N = path_length / self.collision_delta
+		# Check if all configurations along path are obstructed
+		# -----------------------------------------------------------
+
+		return True
+
+
+# Write Your Test Code For Debugging
+#if __name__ == '__main__':
+#	return
+	# Write test code here!
